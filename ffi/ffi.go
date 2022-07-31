84bd1b2e6fcf7356c9044b76864e52d28f687b66
gogogoghost <ohleach@gmail.com>
2022-08-01 00:01:07 +0800

    增加一个测试方法

--- ffi/ffi.go
+++ ffi/ffi.go
@@ -5,18 +5,22 @@ package ffi
 //#include <ffi.h>
 /*
 #include <stdio.h>
-void test(ffi_cif *cif,void(*func)(void),void *result,void **args){
+void ffi_call_test(ffi_cif *cif,void(*func)(void),void *result,void **args){
 	// int* arg = (int*)(args[0]);
 	// printf("%d\n",*arg);
 	// int* res = (int*)result;
 	// *res=999;
 	ffi_call(cif,func,result,args);
 }
+void ffi_prep_cif_test(ffi_type **types){
+	printf("%d\n",types[0]);
+}
 */
 import "C"
 import (
 	"errors"
 	"fmt"
+	"runtime"
 	"unsafe"
 )
 
@@ -40,11 +44,15 @@ type Cif struct {
 }
 
 func NewCif(fPtr unsafe.Pointer, rType C.ffi_type, aTypes ...*C.ffi_type) (cif *Cif, err error) {
-	//申请空间 把cif存到C内存中 对象销毁时请释放！！！！
+	//申请空间 把cif存到C内存中
 	empty_cif := C.ffi_cif{}
 	cif = &Cif{
 		ptr: (*C.ffi_cif)(AllocValOf(empty_cif)),
 	}
+	//对象销毁时释放内存
+	runtime.SetFinalizer(cif, func(cif *Cif) {
+		FreePtr(unsafe.Pointer(cif.ptr))
+	})
 	cif.fPtr = fPtr
 	cif.args_count = len(aTypes)
 	var argsPtr **C.ffi_type
@@ -52,6 +60,7 @@ func NewCif(fPtr unsafe.Pointer, rType C.ffi_type, aTypes ...*C.ffi_type) (cif *
 		argsPtr = AllocArrayOf(aTypes)
 		defer FreePtr(unsafe.Pointer(argsPtr))
 	}
+	C.ffi_prep_cif_test(argsPtr)
 	ret := C.ffi_prep_cif(
 		cif.ptr,
 		C.FFI_DEFAULT_ABI,
@@ -80,7 +89,7 @@ func (cif *Cif) Call(resPtr unsafe.Pointer, args ...any) {
 	// 	argp,
 	// ))
 	// C.test(argp)
-	C.test(
+	C.ffi_call_test(
 		cif.ptr,
 		(*[0]byte)(cif.fPtr),
 		resPtr,
